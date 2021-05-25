a824bfe9e4784110956b0cd259552e3db2f77e1f
Sahin Reja <sahinreja512@gmail.com>
2021-05-25 15:43:49 +0530

    First Commit

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const port = process.env.PORT || 5500;
 app.use(express.static('./public'))
 
 app.get('/',(req,res)=>{
-    res.sendFile(path.join(__dirname , './public/home.html'))
+    res.sendFile(path.join(__dirname , './public/index.html'))
 })
 app.get('/about',(req,res)=>{
     res.sendFile(path.join(__dirname , './public/about.html'))
